0ddfcd2a9292c34e4571652cbd7d0173a09b15c5
classicyamaha <mailtosanaur@yahoo.com>
2018-08-03 09:14:00 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import React, { Component } from 'react';
+import {View} from 'react-native';
+import LoginForm from './components/LoginForm';
+
+
+export default class App extends Component {
+ 
+  render() {
+    return (
+      <View>
+      <LoginForm/>
+      </View>
+    );
+  }
+}
+     
---
+++ src/components/LoginForm.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import {StyleSheet,Text,View, Image,TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+
+
+export default class LoginForm extends Component {
+ 
+    render() {
+      return (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+        <Image 
+        style={styles.logoStyle}
+        source={require('../images/logo.png')}/>
+        <Text style={styles.TextStyle}>Procurement Service</Text>
+        </View>
+    
+        <View style={styles.loginContainer}>
+              <TextInput placeholder="username"
+              placeholderTextColor='#FFF'
+              autoCapitalize="none"
+              autoCorrect={false}
+              onSubmitEditing={()=>this.passwordInput.focus()}
+              style={styles.TextInputStyle}/>
+              <TextInput 
+              placeholder="password"
+              placeholderTextColor='#FFF'
+              secureTextEntry
+              ref={(input)=> this.passwordInput=input}
+              style={styles.TextInputStyle}/>
+              <TouchableOpacity style={styles.ButtonStyle}>
+        <Text style={styles.ButtonTextStyle}>Log In</Text>
+        </TouchableOpacity></View>
+        </KeyboardAvoidingView>
+        
+      
+        
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    ButtonTextStyle:{
+  textAlign:'center',
+  color:'#FFF',
+  fontWeight:'700'
+    },
+    ButtonStyle:{
+      backgroundColor:'#446CB3',
+      paddingVertical:15,
+      marginBottom:10,
+  paddingHorizontal: 10
+    },
+    TextInputStyle: {
+      height: 40,
+      backgroundColor:'#52B3D9',
+      marginBottom: 20,
+      paddingHorizontal: 10,
+      color: '#FFF'
+  },
+  loginContainer:{
+      padding: 20
+  },
+    container: {
+      flex: 1,
+      backgroundColor: '#C5EFF7',
+    },
+    logoContainer:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexGrow:1
+  
+    },
+    logoStyle:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 250,
+      height: 250
+    },
+    TextStyle:{
+      fontSize: 25,
+      color: '#03A678',
+  
+  
+    }
+  
+  });
+  
